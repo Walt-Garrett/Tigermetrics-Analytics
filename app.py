51c27de7a703a9d626c40f34980d57eef120ab58
Walter Garrett <frostyyallday@gmail.com>
2026-09-23 23:36:29 -0500

    Renamed app.py to Home.py and added documentation for site pages

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title("Trinity Baseball Tigermetrics")
-st.markdown("Welcome to the Trinity University Baseball Tigermetrics Lab! See the below sections for guides to each page.")
-st.markdown("---")
-
-st.header("Pitcher Breakdowns")
-st.markdown("The Pitcher Breakdowns Page allows for easy viewing of metrics like average velocity, strike percentage, pitch movement, locations, and spin rate. To start, select a pitcher and date range, then select your desired metrics to display from the dropdown menu.")
-st.subheader("Visualizations Guide")
-st.markdown("")
